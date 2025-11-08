a2dd6d534672c44c9e3a85cba3a779c68a7fb559
KRISHNA PATIL RAJPUT <dhanadai.krishna@gmail.com>
2025-11-08 15:07:03 +0530

    fix: create UserContext inline to fix build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/authContext'
-import { UserProvider } from './context/userContext'
+import { UserContext } from './context/UserContext.jsx';
 import App from './App.jsx'
 import './index.css'
 
