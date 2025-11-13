e228ce03f1805d87a37e83159521b5127e7de5a8
Mahesh Vispute <maheshvispute082@gmail.com>
2025-11-13 17:07:43 +0530

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/authContext'
-import { UserProvider } from './context/userContext'
+
+import { AuthProvider } from './context/authContext.jsx'
+import { UserProvider } from './context/UserContext.jsx'
 import App from './App.jsx'
 import './index.css'
 
