2b3e6d1eae1700f05031b2ef5e73e8e1984cf352
Mahesh Vispute <maheshvispute082@gmail.com>
2025-11-13 17:23:29 +0530

    configs added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1000 // Increase chunk size warning limit
-  },
-  base: '/',
-  publicDir: 'public'
+  base: './',
 })
